b710c9d49cd9119c565fde4b18a440ac6288ecc5
Marshall Woods <80414685+FrontlineGenisis@users.noreply.github.com>
2022-12-13 18:12:10 +0000

    Unnamed commit

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
+// Start Error Logging
+error_reporting(E_ALL); 
+ini_set('ignore_repeated_errors', TRUE); 
+ini_set('display_errors', FALSE); 
+ini_set('log_errors', TRUE); 
+ini_set('error_log', '/errors.log'); 
+
 // Start Page Load Counter
 $start_time = microtime(TRUE);
 
 // Set JSON Headers
 header('Access-Control-Allow-Origin: *');
 header("Content-type: application/json; charset=utf-8");
+header("HTTP/1.1 206 Partial Content");
 
 // Get Fact From TXT List
 $f_contents = file("list.txt");
@@ -20,6 +28,7 @@ $time_taken = round($time_taken,2);
 $RandomFact = new stdClass();
 $RandomFact->fact = str_replace(array("\n", "\r"), '', $line);
 $RandomFact->loadtime = $time_taken;
+$RandomFact->response = http_response_code();
 
 // Send JSON
 $FinalJSON = json_encode($RandomFact);
